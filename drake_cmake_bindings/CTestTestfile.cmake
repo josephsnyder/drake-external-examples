b301c2fac29f904132e4fb44e5536d298e1abb46
Joseph Snyder <joe.snyder@kitware.com>
2020-05-12 17:30:21 -0400

    WIP: Add example for binding

---
+++ drake_cmake_bindings/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/softhat/Work/TRI/drake-external-examples/drake_cmake_bindings
+# Build directory: /home/softhat/Work/TRI/drake-external-examples/drake_cmake_bindings
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
